7b02e852ee533d276fe5224edfe4924dc07222cc
marcoscarvallho <marcosarcarvallho@gmail.com>
2022-04-02 16:48:27 -0300

    faltaumacoisa

--- src/app/add-startup/add-startup.component.ts
+++ src/app/add-startup/add-startup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Startup } from '../startup';
+import { STARTUPS } from '../startup-testes';
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 
 @Component({
   selector: 'app-add-startup',
@@ -6,7 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-startup.component.css']
 })
 export class AddStartupComponent implements OnInit {
+  startup = STARTUPS;
+  clickMessage = '';
+  categorias: string[] = ['Seed Stage', 'Angel Investors', 'Early Stage', 'Series A', 'Series B', 'Series C', 'Mezzanine']
 
+  onClickMe(nome: string, qtdFuncionarios: string, industria: string, localizacao: string, rank: string, descricao: string) {
+    this.startup[STARTUPS.length-1].id = STARTUPS.length+1
+    this.startup[STARTUPS.length-1].name = nome
+    this.startup[STARTUPS.length-1].qtdFuncionarios = 500
+    this.startup[STARTUPS.length-1].industria = industria
+    this.startup[STARTUPS.length-1].localizacao = localizacao
+    this.startup[STARTUPS.length-1].rank = 30
+    this.startup[STARTUPS.length-1].descricao = descricao
+    this.startup[STARTUPS.length-1].categoria = 'Seed Stage'
+    console.log(this.startup[STARTUPS.length-1])
+    STARTUPS.push(this.startup[STARTUPS.length-1]);
+  }
   constructor() { }
 
   ngOnInit(): void {
